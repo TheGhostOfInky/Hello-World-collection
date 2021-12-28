f9ee257ee97821e95ea8266bfc3db65202bbddf9
TheGhostOfInky <theghostofinky@gmail.com>
2021-12-28 10:22:14 +0000

    Added VB.NET

---
+++ infinite-count/hwic.vb
@@ -0,0 +1,11 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        Dim i As Integer = 0
+        Do
+            Console.WriteLine("Hello World " & i)
+            i = i + 1
+        Loop While true
+    End Sub
+End Module
---
+++ infinite-loop/hwil.vb
@@ -0,0 +1,9 @@
+Imports System
+
+Module Program
+    Sub Main(args As String())
+        While true
+            Console.WriteLine("Hello World")
+        End While  
+    End Sub
+End Module
